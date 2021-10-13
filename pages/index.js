40cdc74c635186698aced9e8b341a12b97b5c208
Ivaylo Tsochev <ivo.tsochev@live.com>
2021-10-13 23:29:05 +0300

    removing css from globals and home.modiles; cleaning the homepage and started working on navigation

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// Components
+import Nav from '../components/Nav/Nav';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Headless.Team</title>
         <meta name="description" content="Pro Performance, Pro Attitude" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Headless.Team
-        </h1>
-        <h2>Coming Soon</h2>
+      <main>
+        <Nav />
       </main>
 
       <footer>
