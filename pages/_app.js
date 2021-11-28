0bf737d45ad264e6358cb861d1adaec87f29966b
Ivaylo Tsochev <ivo.tsochev@live.com>
2021-11-28 20:44:20 +0200

    adding HEAD tag in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -21,18 +21,18 @@ function MyApp ({ Component, pageProps }) {
     // from the event with the `off` method
     return () => {
 
-      // <Head>
-      //   <meta charset='utf-8' />
-      //   <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-      //   <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-      //   <meta name='description' content='Description' />
-      //   <title>Next.js PWA</title>
-      //   <link rel='manifest' href='/manifest.json' />
-      //   <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-      //   <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-      //   <link rel='apple-touch-icon' href='/logo1.png'></link>
-      //   <meta name='theme-color' content='#21125e' />
-      // </Head>
+      <Head>
+        <meta charset='utf-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+        <meta name='description' content='Description' />
+        <title>Headless Team</title>
+        <link rel='manifest' href='/manifest.json' />
+        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+        <link rel='apple-touch-icon' href='/logo1.png'></link>
+        <meta name='theme-color' content='#21125e' />
+      </Head>
 
 
       router.events.off('routeChangeComplete', handleRouteChange)
